a21c46c19fbd53b5b85927057890be12ba06e954
Bram Wasti <bwasti@gmail.com>
2015-02-07 02:26:21 -0500

    added updated header

--- src/inc/p2plib.h
+++ src/inc/p2plib.h
@@ -17,6 +17,8 @@ int p2p_connect(char *server, char *port, connection_t *con);
 
 int p2p_send(connection_t *con, void *buf, size_t buflen);
 
+int p2p_broadcast(connection_t **cons, size_t *conslen, pthread_mutex_t *consmutex, void *buf, size_t buflen);
+
 int p2p_listener(connection_t **cons, size_t *conslen,
                  pthread_mutex_t *consmutex,
                  void (*callback)(connection_t *, void *, size_t),
