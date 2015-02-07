4da98a4361300edeabe7a208f89f3e3ef35d7c48
Stefan Dasbach <StefanDasbach@gmail.com>
2015-02-07 13:22:27 -0500

    Initial video with color

--- src/display.c
+++ src/display.c
@@ -3,22 +3,21 @@
 WINDOW *main_screen;
 
 /* private functions */
-void n3_init_colors(void);
+void init_colors(void);
 
-void n3_init_screen(void){
+void init_screen(void){
   main_screen = initscr();
   keypad(stdscr, TRUE);           // keypad enabled
   (void) nodelay(main_screen, 1); // no blocking
-  (void) nonl();                  // no new lines
   (void) noecho();                // don't print to screen
-  n3_init_colors();
-  refresh();
+  (void) nonl();                  // no new lines
+  init_colors();
 }
 
 /*
  * crazy shifting is to set up every color 
  */
-void n3_init_colors(void) {
+void init_colors(void) {
   start_color();
   for (int i = 0; i < (1 << 8); i ++) {
     int r = i >> 5;
@@ -30,7 +29,7 @@ void n3_init_colors(void) {
   return;
 }
 
-void n3_end_screen(void) {
+void end_screen(void) {
   endwin();
 }
 
@@ -39,36 +38,28 @@ static inline int get_color(int r, int g, int b) {
   return 16+r/48*36+g/48*6+b/48; 
 }
 
-const char *sizes = "=+*%%@#";
-
+const char *ascii_values = " .:-=+oo*#%@";
 /* vector drawer
  */
-int n3_draw_image(char *data, int width, int height) {
-  for (int y=0; y<height; y++){
-    for (int x=0; x<width; x++){
-      int intensity = (data[((height-y-1)*width+x)*4] +
-          data[((height-y-1)*width+x)*4+1] + 
-          data[((height-y-1)*width+x)*4+2])/127;
-      int color = get_color(
-          data[((height-y-1)*width+x)*4],
-          data[((height-y-1)*width+x)*4+1],
-          data[((height-y-1)*width+x)*4+2]);
-      //mvaddch(y, x, sizes[intensity]|COLOR_PAIR(color));
-
+//int n3_draw_image(char *data, int width, int height) {
+int draw_image(char *data, int width, int height, int step, int channels) {
+  char ascii_image[width*height];
+  int y, x;
+  unsigned char b, g, r;
+  int offset = 0;
+  int intensity;
+  for (y=0; y<height; y++){
+    for (x=0; x<width; x++){
+      b = data[step * y + x * channels] + offset;
+      g = data[step * y + x * channels + 1] + offset;
+      r = data[step * y + x * channels + 2] + offset;
+      intensity = (int)(0.2126*r + 0.7152*g + 0.0722*b);
+      ascii_image[y*width + x] = ascii_values[intensity / 25];
+      int color = get_color(r,g,b);
+      mvaddch(y, x, ascii_image[y*width + x]|COLOR_PAIR(color));
     }
-  }
-  return 0;
+  } 
+
+  refresh();
 }
 
-/* 
- * draws char to (x,y)
- * returns -1 on error
- */
-int n3_draw_xy(char c, int x, int y){
-  if (x > COLS || x < 0 || y > LINES || y < 0) {
-    return -1;
-  } else {
-    mvaddch(x, y, c);
-    return 0;
-  }
-}
--- src/video.c
+++ src/video.c
@@ -3,15 +3,18 @@
 #include <stdio.h>
 #include "display.h"
 
-#define VIDEO_WIDTH 200
-#define VIDEO_HEIGHT 250
+#define VIDEO_WIDTH 100
+#define VIDEO_HEIGHT 40
 
-//#ifdef VIDEOONLY
+#ifdef VIDEOONLY
+
 
 int main(void) {
+  init_screen();
+
   IplImage* color_img;
-  IplImage* resize_img = cvCreateImage(cvSize(VIDEO_WIDTH,VIDEO_HEIGHT),8,3)    ;   
-  IplImage* gray_img = cvCreateImage(cvSize(VIDEO_WIDTH,VIDEO_HEIGHT),8,1)    ;   
+  IplImage* resize_img = cvCreateImage(cvSize(VIDEO_WIDTH,VIDEO_HEIGHT),8,3);  
+
   CvCapture* cv_cap = cvCaptureFromCAM(0);
   /* Create window */
   cvNamedWindow("Video",0); 
@@ -20,15 +23,16 @@ int main(void) {
     color_img = cvQueryFrame(cv_cap);
     if(color_img != 0) {
       cvResize(color_img, resize_img, CV_INTER_AREA);
-      cvCvtColor(resize_img, gray_img, CV_BGR2GRAY);
       /* Display it yo */
-      cvShowImage("Video", gray_img);
-      n3_draw_image(gray_img->imageData, gray_img->width, gray_img->height);
+      cvShowImage("Video", resize_img);
+
+      draw_image(resize_img->imageData, resize_img->width, resize_img->height, resize_img->widthStep, resize_img->nChannels);
     }
   }
   /* Housekeeping */
   cvReleaseCapture( &cv_cap );
   cvDestroyWindow("Video");
+  end_screen();
   return 0;
 }
-//#endif
+#endif
