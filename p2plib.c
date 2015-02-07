764eaeb4fe535f4ae3580a70f8b1b5376d8161df
cpd85 <cpdonaghy@gmail.com>
2015-02-06 23:25:43 -0500

    udp init

---
+++ p2plib.c
@@ -0,0 +1,17 @@
+
+
+
+/* initializes a UDP server, returns 0 on success or a corresponding errno
+ * on failure */
+int init_server
+
+/* sample code which can hopefully write and read to another peer */
+int main(int argc, char **argv) {
+  char *testmesg = "hello";
+  
+  char *port = getenv("peerport\n");
+  if
+
+
+
+}
